62604a048f73cb81f09fe6f2b649fbeabcf243ee
steven <stevenson.celi6741@utc.edu.ec>
2026-07-29 17:23:28 -0500

    superusuario

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('stevenson', 'estivgamer115@gmail.com', '123456')"
