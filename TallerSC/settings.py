63c51f439ddbcf0ef86edb87cf20694c9efcd009
steven <stevenson.celi6741@utc.edu.ec>
2026-07-29 19:45:38 -0500

    fechas en guardarOrde4

--- TallerSC/settings.py
+++ TallerSC/settings.py
@@ -154,6 +154,7 @@ EMAIL_BACKEND       = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.cons
 EMAIL_HOST          = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT          = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS       = os.getenv('EMAIL_USE_TLS', 'True').lower() in ('1', 'true', 'yes', 'on')
+EMAIL_USE_SSL       = os.getenv('EMAIL_USE_SSL', 'False').lower() in ('1', 'true', 'yes', 'on')
 EMAIL_HOST_USER     = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL', 'webmaster@localhost')
